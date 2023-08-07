3cb582e742d6f1c006d75e67780d3a2319e052e8
lovesion1 <lovesion1@nate.com>
2023-08-07 10:48:45 +0900

    eu16

--- layout-완성/E-1_김치이야기-완성/script/script.js
+++ layout-완성/E-1_김치이야기-완성/script/script.js
@@ -1,27 +1,52 @@
 $(function(){
+
   const $mainmenu = $(".mainmenu li")
-  const $submenu = $(".submenu")
+  const $submenu = (".submenu")
 
   $mainmenu.hover(function(){
     $(this).find($submenu).stop().slideToggle()
   })
 
-  const $slider = $(".sliderWrap")
-  let index = 0
-  setInterval(function(){
-    index = (index +1 ) % 3
-    $slider.animate({
-      marginLeft : index * - 100 + "%"
-    },300) //marginLeft변경 속도
-  },3000) //이미지 변경 속도
 
-  const $modal = $("#modalArea")
-  const $li = $(".notice li:first-child")
-  const $btn = $(".modal .btn")
-  $li.click(function(){
-    $modal.fadeIn()
-  })
-  $btn.click(function(){
-    $modal.fadeOut()
-  })
+/* $(".slider li:gt(0)").hide()
+setInterval(function(){
+  $(".slider li:first-child").fadeOut()
+  .next("li").fadeIn()
+  .end().appendTo(".slider")
+},3000) */
+
+// 좌우 이동
+let index = 0
+const slider = $(".slider")
+
+setInterval(function(){
+  index = (index +1) % 3
+  slider.animate({
+    marginLeft :index * -100 + "%"
+  },300)
+},3000)
+
+/* // 위아래 이동
+let index = 0
+const $height = $(".slider li").height()
+const slider = $(".slider")
+
+setInterval(function(){
+  index = (index +1) % 3
+  slider.animate({
+    marginTop :index * -$height
+  },300)
+},3000) */
+
+
+const $modal = $("#modalArea")
+const $li = $(".notice li:first-child")
+const $btn = $(".modal .btn")
+$li.click(function(){
+  $modal.fadeIn()
+})
+$btn.click(function(){
+  $modal.fadeOut()
+})
+
 })
