389ba15c18e5d738b0bbb3d335e50f7fa19d6d93
lovesion1 <lovesion1@nate.com>
2023-08-07 12:35:02 +0900

    eu17

--- layout-완성/A-1_just쇼핑몰-완성/script/script.js
+++ layout-완성/A-1_just쇼핑몰-완성/script/script.js
@@ -1,38 +1,70 @@
 $(function(){
-  let index = 0;
+  const $mainMenu = $(".mainmenu")
+  // const $mainMenu = $(".mainmenu li")
+  // 각각 나오게 하기 위함
+  const $subMenu = $(".submenu")
+  const $header =$("header")
+  $mainMenu.hover(function(){
+    $subMenu.stop().fadeToggle(500);
+    $header.stop().toggleClass("on")
+  }) 
+  // 메인 메뉴 하나만 클릭해도 모두 보여지는 장면
+
+
+  /* $mainMenu.hover(function(){
+    $(this).find($subMenu).stop().fadeToggle(500)
+  })
+  // 각각 메뉴 나오게 하는 방법 */
+
+
+
+  // 슬라이드 영역
+  // 좌우 슬라이드 이동
+/*   let index = 0
   const slider = $(".slider")
 
-  function slide() {
-    index = (index + 1) % 3;
+  setInterval(function(){
+    index = (index + 1) % 3
 
     slider.animate({
-      marginTop: index * -300
-    }, 500)
-  }
-  setInterval(slide, 3000)
-  // 3개의 슬라이드 위아래로 이동
+      marginLeft : index * -1200
+    },500)
+  },3000) */
 
 
-  const $mainMenu = $(".mainmenu")
-  const $subMenu = $(".submenu")
-  $mainMenu.hover(function(){
-    $subMenu.stop().fadeToggle(500)
-  })
-  // 부드럽게 서브메뉴 나왔다가 사라짐
+  /* // 위아래 슬라이드 이동
+  let index = 0
+  const slider = $(".slider")
+
+  setInterval(function(){
+    index = (index + 1) % 3
+
+    slider.animate({
+      marginTop : index * -300
+    },500)
+  },3000) */
 
 
-  $(".tabmenu>li>a").click(function(){
-    $(this).parent().addClass("active").siblings().removeClass("active");
-  });
-  // 각 탭 클릭시 해당 탱에 대한 내용 보여짐
-  
-  $(".notice ul > li:first").click(function(){
-    $("#popupArea").show();
-  });
-  $("#popupArea .btn").click(function(){
-    $("#popupArea").hide();
-  })
-  // 공지사항의 첫번째 콘텐츠 클릭시 팝업창 나타나고 닫기버튼으로 닫힘
+    // 패더로 슬라이드 이동
+    $(".slider li:gt(0)").hide();
+    setInterval(function(){
+      $(".slider li:first-child").fadeOut()
+      .next('li').fadeIn()
+      .end().appendTo(".slider")
+
+    },3000)
+
+
+    $(".tabmenu>li").click(function(){
+      $(this).addClass("active").siblings().removeClass("active")
+    })
+
+    $(".notice ul>li:first").click(function(){
+      $("#modalArea").fadeIn();
+    })
+    $("#modalArea .btn").click(function(){
+      $("#modalArea").fadeOut()
+    })
 
 
 })
